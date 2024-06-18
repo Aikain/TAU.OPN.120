7c68041bc0ef1971de937519007c009d0a33854b
Ville Nupponen <ville.nupponen@aika.in>
2024-06-18 14:31:25 +0300

    Add 4.1: Vertex AI: Gemini & PaLM2

---
+++ thesis/src/code/GeminiService.java
@@ -0,0 +1,38 @@
+@Override
+public List<StoryLine> createStoryLines(Story story) {
+    GenerativeModel model = new GenerativeModel(
+            story.getModelName().getName(),
+            vertexAI
+    );
+    ChatSession chatSession = model.startChat();
+    chatSession.withGenerationConfig(createGenerationConfig());
+    chatSessions.put(story.getId(), chatSession);
+    try {
+        GenerateContentResponse response =
+            chatSession.sendMessage(createInitializeContent(story));
+        String text =
+            response.getCandidates(0).getContent().getParts(0).getText();
+        return objectMapper.readValue(text, new TypeReference<>() {});
+    } catch (IOException e) {
+        log.warn("Failed to generate story: {}", e.getMessage());
+        throw new InternalServerErrorException("Failed to generate story");
+    }
+}
+
+@Override
+public List<StoryLine> continueStoryLines(Story story) {
+    if (!chatSessions.containsKey(story.getId())) throw new GoneException();
+    ChatSession chatSession = chatSessions.get(story.getId());
+    try {
+        GenerateContentResponse response =
+            chatSession.sendMessage("Jatka tarinaa");
+        String text =
+            response.getCandidates(0).getContent().getParts(0).getText();
+        return objectMapper.readValue(text, new TypeReference<>() {});
+    } catch (IOException e) {
+        log.warn("Failed to generate story continuation: {}", e.getMessage());
+        throw new InternalServerErrorException(
+            "Failed to generate story continuation"
+        );
+    }
+}
