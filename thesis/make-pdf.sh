dfa658b0582e193ad657fec45fae2418f1d9be42
Ville Nupponen <ville.nupponen@tuni.fi>
2025-01-12 13:00:08 +0200

    Fix latex warning

--- thesis/make-pdf.sh
+++ thesis/make-pdf.sh
@@ -2,6 +2,11 @@
 
 cd "$(dirname "$0")/src"
 
+# https://tex.stackexchange.com/a/52994
+export max_print_line=1000
+export error_line=254
+export half_error_line=238
+
 pdflatex -output-directory=../out main.tex
 
 cd ../out
